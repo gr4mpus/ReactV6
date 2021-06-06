44682c3c0324354f0c56f034935ef93f35ac1e89
Rahul Prasad <rahul.prsd.in@gmail.com>
2021-06-06 08:07:34 +0530

    npm setup

--- src/app.js
+++ src/app.js
@@ -1,29 +1,14 @@
-const Pet = (props) =>(
-    React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h2',{}, props.name),
-            React.createElement('h3',{}, props.type),
-            React.createElement('h3',{}, props.breed),
-        ]
-    )
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './pet';
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me"),
-            React.createElement(Pet, {name: 'Luna', type:'Dog', breed:'Havanese'}),
-            React.createElement(Pet, {name: 'Pepper', type:'Bird', breed:'Parrot'}),
-            React.createElement(Pet, {name: 'Sudo', type:'Dog', breed:'Pug'}),
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, { name: "Luna", type: "Dog", breed: "Havanese" }),
+    React.createElement(Pet, { name: "Pepper", type: "Bird", breed: "Parrot" }),
+    React.createElement(Pet, { name: "Sudo", type: "Dog", breed: "Pug" }),
+  ]);
+};
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
