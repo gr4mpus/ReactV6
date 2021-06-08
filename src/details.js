f5472481cf4a5a35c76b3b16ec34ea6205cb3027
Rahul Prasad <rahul.prsd.in@gmail.com>
2021-06-08 07:06:19 +0530

    class component starting with lifecycle hook

--- src/details.js
+++ src/details.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { React, Component } from "react";
+import { withRouter } from "react-router-dom";
 
-const Details = () => {
-  return <h2>HI</h2>;
-};
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
 
-export default Details;
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    console.log(this.state);
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// withRouter is going to pass the details to Details component. TODO
+export default withRouter(Details);
